e88bbffa9f2439d18414da0bc9ce5061ef275959
lmorais <lmorais.contato@gmail.com>
2023-07-19 22:47:56 -0300

    chore: change import ordenation in nodemailer email service

--- src/external/mail-services/nodemailer-email-service.ts
+++ src/external/mail-services/nodemailer-email-service.ts
@@ -1,7 +1,7 @@
+import * as nodemailer from 'nodemailer';
 import { Either, left, right } from "@/shared/either";
 import { MailServiceError } from "@/usecases/errors/mail-service-error";
 import { EmailOptions, EmailService } from "@/usecases/send-email/ports/email-service";
-import * as nodemailer from 'nodemailer';
 
 export class NodemailerEmailService implements EmailService {
   async send (options: EmailOptions): Promise<Either<MailServiceError, EmailOptions>> {
