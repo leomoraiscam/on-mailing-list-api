fdad197766d9e3ea708b66c38b2bc4ce0fc511e1
lmorais <lmorais.contato@gmail.com>
2023-07-19 23:53:24 -0300

    chore: remove unused imports in usecase

--- src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.ts
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.ts
@@ -1,9 +1,6 @@
 import { User } from '@/entities/user';
 import { UserData } from '@/dtos/user-data';
 import { UserRepository } from './ports/user-repository';
-import { Either, left, right } from '@/shared/either';
-import { InvalidEmailError } from '@/entities/errors/invalid-email-error';
-import { InvalidNameError } from '@/entities/errors/invalid-name-error';
 import { UseCase } from '../ports/use-case';
 
 export class RegisterUserOnMailingList implements UseCase {
