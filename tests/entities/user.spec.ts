68ea4af98072b7f2cca9f0b1c0f665ffe61c8871
lmorais <lmorais.contato@gmail.com>
2023-07-19 22:37:33 -0300

    test:change variables inside user.spec

--- tests/entities/user.spec.ts
+++ tests/entities/user.spec.ts
@@ -2,27 +2,27 @@ import { User } from "@/entities/user";
 
 describe('User domain class', () => {
   it('should not create user with invalid email address', () => {
-    const invalid_email = 'invalid_email';
+    const invalidEmail = 'local_domain.com';
 
     const error = User.create({
-      name: 'any_name',
-      email: invalid_email
+      name: 'John doe',
+      email: invalidEmail
     }).value as Error;
 
     expect(error.name).toEqual('InvalidEmailError');
-    expect(error.message).toEqual(`Invalid email: ${invalid_email}.`);
+    expect(error.message).toEqual(`Invalid email: ${invalidEmail}.`);
   })
 
   it('should not create user with invalid name (too few characters)', () => {
-    const invalid_name = 'l       ';
+    const invalidName = 'l       ';
 
     const error = User.create({ 
-      name: invalid_name, 
-      email: 'any@mail.com' 
+      name: invalidName, 
+      email: 'local@domain.com' 
     }).value as Error;
 
     expect(error.name).toEqual('InvalidNameError');
-    expect(error.message).toEqual(`Invalid name: ${invalid_name}.`);
+    expect(error.message).toEqual(`Invalid name: ${invalidName}.`);
   })
 
   it('should not create user with invalid name (too many characters)', () => {
@@ -30,20 +30,21 @@ describe('User domain class', () => {
 
     const error = User.create({
       name: invalid_name,
-      email: 'any_email@email.com'
+      email: 'local_partl@domain.com'
     }).value as Error;
 
     expect(error.name).toEqual('InvalidNameError');
-    expect(error.message).toEqual(`Invalid name: ${invalid_name}.`);  })
+    expect(error.message).toEqual(`Invalid name: ${invalid_name}.`);  
+  })
 
   it('should create user with valid data', () => {
     const user = User.create({
       name: 'any',
-      email: 'any_email@email.com'
+      email: 'local_partl@domain.com'
     }).value as User;
 
     expect(user.name.value).toEqual('any')
-    expect(user.email.value).toEqual('any_email@email.com')
+    expect(user.email.value).toEqual('local_partl@domain.com')
 
   })
 })
