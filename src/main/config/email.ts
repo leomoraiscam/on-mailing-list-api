495108da474baa031864f3e804271d3b1d0b1efa
lmorais <lmorais.contato@gmail.com>
2023-07-19 23:25:35 -0300

    refactor: improve content email in main configuration mailer

--- src/main/config/email.ts
+++ src/main/config/email.ts
@@ -1,13 +1,17 @@
-import { EmailOptions } from '@/usecases/send-email/ports/email-service'
+import { EmailOptions } from '@/usecases/send-email/ports/email-service';
 
 const attachments = [{
   filename: 'clean-architecture.pdf',
   path: 'https://otaviolemos.github.io/clean-architecture.pdf'
 }]
 
+const fromName = 'MailingList';
+const fromEmail = 'mainling_list_contact@mail.com';
+
 export function getEmailOptions (): EmailOptions {
-  const from = 'Leonardo morais | theWiseDev <lmoraiss@gmail.com>'
+  const from = `${fromName} | theWiseDev <${fromEmail}>`
   const to = ''
+  
   const mailOptions: EmailOptions = {
     host: 'smtp.mailtrap.io',
     port: 2525,
@@ -15,9 +19,9 @@ export function getEmailOptions (): EmailOptions {
     password: '4c510740359934',
     from: from,
     to: to,
-    subject: 'Mensagem de teste',
-    text: 'Texto da mensagem',
-    html: '<b> Texto da mensagem </b>',
+    subject: 'Attachments e-mail',
+    text: 'Hello world attachment test',
+    html: '<b>Hello world attachment test</b>',
     attachments: attachments
   }
   return mailOptions
