d9ee14b2e48a8ddcc9b69dc3924c4274b9d6805c
lmorais <lmorais.contato@gmail.com>
2023-07-27 17:19:41 -0300

    refactor: adapt controller register user on mailing list and send email with a new types

--- src/web-controllers/register-user-and-send-email-controller.ts
+++ src/web-controllers/register-user-and-send-email-controller.ts
@@ -4,6 +4,7 @@ import { MissingParamError } from './errors/missing-param-error';
 import { HttpRequest } from './ports/http-request';
 import { HttpResponse } from './ports/http-response';
 import { badRequest, created, serverError } from './utils/http-helper';
+import { User } from '@/entities/user';
 
 export class RegisterUserAndSendEmailController {
   private readonly usecase: UseCase;
@@ -12,26 +13,26 @@ export class RegisterUserAndSendEmailController {
     this.usecase = usecase;
   }
 
-  async handle(request: HttpRequest): Promise<HttpResponse> {  
+  async handle(request: HttpRequest<UserData | Partial<UserData>>): Promise<HttpResponse<UserData | Error>> {  
     try {
       if (!request.body.name || !request.body.email) {
         let missing = !request.body.name ? 'name ' : '';
 
         missing += !request.body.email ? 'email' : '';
 
-        return badRequest(new MissingParamError(missing.trim()));
+        return badRequest<Error>(new MissingParamError(missing.trim()));
       }
 
-      const userData: UserData = request.body;
+      const userData = request.body as UserData;
       const response = await this.usecase.perform(userData);
 
       if (response.isLeft()) {
-        return badRequest(response.value);
+        return badRequest<Error>(response.value);
       }
 
-      return created(response.value);
+      return created<UserData>(response.value);
     } catch (error) {
-      return serverError(error);
+      return serverError<Error>(error);
     }
   }
 }
