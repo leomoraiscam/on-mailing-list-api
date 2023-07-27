ccd2afab9a020ba2dd4dd411b7ecdef0915dc00e
lmorais <lmorais.contato@gmail.com>
2023-07-27 17:21:10 -0300

    refactor: change mail service error

--- src/usecases/errors/mail-service-error.ts
+++ src/usecases/errors/mail-service-error.ts
@@ -1,6 +1,7 @@
 export class MailServiceError extends Error {
+  public readonly name: string = 'MailServiceError';
+  
   constructor() {
     super('Mail service error.');
-    this.name = 'MailServiceError';
   }
 }
