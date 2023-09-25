4b8168a24e3608466f7ca0166e712c6e065eb355
lmorais <lmorais.contato@gmail.com>
2023-09-25 15:21:50 -0300

    test: configuration jest folder and files

--- jest/jest-unit-config.js
+++ jest/jest-unit-config.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-unresolved */
 /* eslint-disable @typescript-eslint/no-var-requires */
 // eslint-disable-next-line import/extensions
 const config = require('../jest.config');
