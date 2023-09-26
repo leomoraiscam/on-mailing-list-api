782eba931e9323daa04a145d029c15cac93d2ed7
lmorais <lmorais.contato@gmail.com>
2023-09-25 23:13:46 -0300

    refactor: add infere type in user domain entity

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -3,6 +3,7 @@ import { Either, left, right } from '@/shared/either';
 
 import { Email } from './email';
 import { InvalidEmailError } from './errors/invalid-email-error';
+import { InvalidNameError } from './errors/invalid-name-error';
 import { Name } from './name';
 
 export class User {
@@ -16,20 +17,24 @@ export class User {
   }
 
   static create(userData: UserData): Either<InvalidEmailError, User> {
-    const nameOrError = Name.create(userData.name);
+    const nameOrError: Either<InvalidNameError, Name> = Name.create(
+      userData.name
+    );
 
     if (nameOrError.isLeft()) {
       return left(nameOrError.value);
     }
 
-    const emailOrError = Email.create(userData.email);
+    const emailOrError: Either<InvalidEmailError, Email> = Email.create(
+      userData.email
+    );
 
     if (emailOrError.isLeft()) {
       return left(emailOrError.value);
     }
 
-    const name: Name = nameOrError.value as Name;
-    const email: Email = emailOrError.value as Email;
+    const name: Name = nameOrError.value;
+    const email: Email = emailOrError.value;
 
     return right(new User(name, email));
   }
--- tests/entities/user.spec.ts
+++ tests/entities/user.spec.ts
@@ -1,13 +1,27 @@
 import { User } from '@/entities/user';
 
 describe('User domain class', () => {
+  it('should create user with valid data', () => {
+    const userOrError = User.create({
+      name: 'any',
+      email: 'local_partl@domain.com',
+    });
+
+    const user = userOrError.value as User;
+
+    expect(user.name.value).toEqual('any');
+    expect(user.email.value).toEqual('local_partl@domain.com');
+  });
+
   it('should not create user with invalid email address', () => {
     const invalidEmail = 'local_domain.com';
 
-    const error = User.create({
+    const userOrError = User.create({
       name: 'John doe',
       email: invalidEmail,
-    }).value as Error;
+    });
+
+    const error = userOrError.value as Error;
 
     expect(error.name).toEqual('InvalidEmailError');
     expect(error.message).toEqual(`Invalid email: ${invalidEmail}.`);
@@ -16,10 +30,12 @@ describe('User domain class', () => {
   it('should not create user with invalid name (too few characters)', () => {
     const invalidName = 'l       ';
 
-    const error = User.create({
+    const userOrError = User.create({
       name: invalidName,
       email: 'local@domain.com',
-    }).value as Error;
+    });
+
+    const error = userOrError.value as Error;
 
     expect(error.name).toEqual('InvalidNameError');
     expect(error.message).toEqual(`Invalid name: ${invalidName}.`);
@@ -28,22 +44,14 @@ describe('User domain class', () => {
   it('should not create user with invalid name (too many characters)', () => {
     const invalid_name = 'l'.repeat(257);
 
-    const error = User.create({
+    const userOrError = User.create({
       name: invalid_name,
       email: 'local_partl@domain.com',
-    }).value as Error;
+    });
+
+    const error = userOrError.value as Error;
 
     expect(error.name).toEqual('InvalidNameError');
     expect(error.message).toEqual(`Invalid name: ${invalid_name}.`);
-  });
-
-  it('should create user with valid data', () => {
-    const user = User.create({
-      name: 'any',
-      email: 'local_partl@domain.com',
-    }).value as User;
-
-    expect(user.name.value).toEqual('any');
-    expect(user.email.value).toEqual('local_partl@domain.com');
   });
 });
