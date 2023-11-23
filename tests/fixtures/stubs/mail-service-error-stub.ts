475ef6c967e455f6bc23c4be294f148b9b795793
lmorais <lmorais.contato@gmail.com>
2023-11-23 17:49:03 -0300

    fix: adjust mail service error stubs

--- tests/fixtures/stubs/mail-service-error-stub.ts
+++ tests/fixtures/stubs/mail-service-error-stub.ts
@@ -4,12 +4,7 @@ import { MailServiceError } from '@/usecases/errors/mail-service-error';
 import { EmailService } from '@/usecases/send-email/ports/email-service';
 
 export class MailServiceErrorStub implements EmailService {
-  // Como se fosse o envio de e-mail em memoria, porem simulando o erro
-  public timesSendWasCalled = 0;
-
-  async send(
-    emailOptions: EmailOptions
-  ): Promise<Either<MailServiceError, EmailOptions>> {
+  async send(_: EmailOptions): Promise<Either<MailServiceError, EmailOptions>> {
     return left(new MailServiceError());
   }
 }
